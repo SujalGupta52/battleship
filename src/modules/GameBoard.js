726a6ede26769c428784b8d9bad68b2afcd36de4
SujalGupta52 <67575635+SujalGupta52@users.noreply.github.com>
2023-12-29 21:20:49 +0530

    Add placeShip function to gameboard and create required test

--- src/modules/GameBoard.js
+++ src/modules/GameBoard.js
@@ -6,10 +6,10 @@ export default class Gameboard {
 
     const gameboardArray = [];
     let i = 0;
-    while (i < 8) {
+    while (i < size) {
       const temp = [];
       let j = 0;
-      while (j < 8) {
+      while (j < size) {
         temp.push(-1);
         j += 1;
       }
@@ -21,7 +21,46 @@ export default class Gameboard {
   }
 
   getBoard() {
-     return this.gameboardArray;
+    return this.gameboardArray;
   }
 
+  checkValidPlacement(size, rotation, coordinate) {
+    const mid = Math.floor(size / 2);
+    if (size > this.size) return false;
+    if (
+      coordinate[0] > this.size ||
+      coordinate[0] < 0 ||
+      coordinate[1] > this.size ||
+      coordinate[1] < 0
+    )
+      return false;
+    if (rotation === "h") {
+      if (coordinate[1] - mid < 0 || coordinate[1] + (size - mid) > this.size)
+        return false;
+    }
+
+    if (rotation === "v") {
+      if (coordinate[0] - mid < 0 || coordinate[0] + (size - mid) > this.size)
+        return false;
+    }
+
+    return true;
+  }
+
+  placeShip(size, rotation, coordinate) {// size: size of ship, rotation: horizontal('h') or vertical('v'), coordinate: [x,y]
+    const ship = new Ship(size);
+    const mid = Math.floor(size / 2)
+    if(this.checkValidPlacement(size, rotation, coordinate)) {
+      if(rotation === "h") {
+        for(let i = coordinate[1] - mid; i < coordinate[1] + (size - mid); i += 1) {
+          this.gameboardArray[coordinate[0]][i] = ship;
+        }
+      }
+      else {
+        for(let i = coordinate[0] - mid; i < coordinate[0] + (size - mid); i += 1) {
+          this.gameboardArray[i][coordinate[0]] = ship;
+        }
+      }
+    }
+  }
 }
--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,25 +1,43 @@
 import Gameboard from "../modules/GameBoard";
 
-let gameboard = new Gameboard();
+let gameboard = new Gameboard(8);
+let mockBoard = (() => {
+  let i = 0;
+  let mockArray = [];
+  while (i < 8) {
+    let temp = [];
+    let j = 0;
+    while (j < 8) {
+      temp.push(-1);
+      j += 1;
+    }
+    mockArray.push(temp);
+    i += 1;
+  }
+  return mockArray;
+})();
 
-test('Gameboard object created', () => {
-    expect(gameboard).not.toBeUndefined();
+test("Gameboard object created", () => {
+  expect(gameboard).not.toBeUndefined();
 });
 
-test('Gameboard array created', () => {
-    let i = 0;
-    let mockArray = []
-    while(i < 8) {
-        let temp = [];
-        let j = 0;
-        while(j < 8) {
-            temp.push(-1);
-            j += 1;
-        }
-        mockArray.push(temp);
-        i += 1;
-    }
-    expect(gameboard.getBoard()).toEqual(mockArray);
+test("Gameboard array created", () => {
+  expect(gameboard.getBoard()).toEqual(mockBoard);
+});
+
+describe("Valid placement", () => {
+
+    test("Coordinate out of board", () => {
+      expect(gameboard.checkValidPlacement(5, "h", [0, 9])).toBe(false);
+    });
+
+    test("Horizontal: Ship is placed in valid coordinate", () => {
+        expect(gameboard.checkValidPlacement(5, "h", [4, 4])).toBe(true);
+    });
+
+    test("Vertical: Ship is placed in valid coordinate", () => {
+          expect(gameboard.checkValidPlacement(5, "v", [4, 4])).toBe(true);
+    });
 });
 
 
