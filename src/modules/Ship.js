016a13f79cc45e75f1ebd4e253cba85157a068bc
SujalGupta52 <67575635+SujalGupta52@users.noreply.github.com>
2023-12-29 13:31:50 +0530

    Create ship class

---
+++ src/modules/Ship.js
@@ -0,0 +1,14 @@
+export default class Ship {
+    constructor(length) {
+        this.length = length;
+        this.health = length;
+    }
+
+    hit() {
+        this.health -= 1;
+    }
+
+    isSunk() {
+        return this.health === 0;
+    }
+}
---
+++ src/tests/ship.test.js
@@ -0,0 +1,17 @@
+import Ship from  '../modules/Ship'
+
+const ship = new Ship(2)
+
+test('Object created successfully', () => {
+    expect(ship).not.toBeUndefined();
+});
+
+test('Hit ship', () => {
+    ship.hit();
+    expect(ship.health).toBe(1);
+});
+
+test('Ship sunk', () => {
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+});
